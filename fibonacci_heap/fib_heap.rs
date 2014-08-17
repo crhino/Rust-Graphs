4b1c0b513fa1fce1a20f5e2454e3985b9fb0e4ac
crhino <piraino.chris@gmail.com>
2014-08-17 00:58:00 -0400

    Fixed some errors from rustc.
    Still not passing rustc though.

--- fibonacci_heap/fib_heap.rs
+++ fibonacci_heap/fib_heap.rs
@@ -3,36 +3,99 @@
  *
  * Fibonacci Heap
  */
+#![crate_name = "fib_heap"]
+#![crate_type = "lib"]
+
+extern crate collections;
 use std::rc::Rc;
-use std::rc::Rc::Weak;
+use std::rc::Weak;
 use collections::dlist::DList;
+use collections::Deque;
 
-pub type FibEntry<K,V> Rc<FibNode<K,V>>
+pub type FibEntry<K,V> = Rc<FibNode<K,V>>;
 
+// Have a trait so that we can implement methods on FibEntry. (error E0116])
+// Can I do this a different way?
+trait FibHeapEntry {
+    fn rank(&self) -> uint;
+}
+
+impl<K,V> FibHeapEntry for FibEntry<K,V> {
+    fn rank(&self) -> uint {
+        self.deref().children.len()
+    }
+}
+
+#[deriving(Clone)]
 struct FibNode<K,V> {
-    parent: Weak<FibNode>,
-    children: DList<FibEntry>,
-    rank: int,
+    parent: Weak<FibNode<K,V>>,
+    children: DList<FibEntry<K,V>>,
+    // Rank is the length of children
     marked: bool,
     key: K,
     value: V
 }
 
-pub struct FibHeap<K,V> {
-    roots: DList<FibEntry>
+impl<K: PartialOrd,V> PartialOrd for FibNode<K,V> {
+    fn partial_cmp(&self, other: &FibNode<K,V>) -> Option<Ordering> {
+        self.key.partial_cmp(&other.key)
+    }
 }
 
-impl<K,V> FibHeap<K,V> {
+impl<K: PartialEq,V> PartialEq for FibNode<K,V> {
+    fn eq(&self, other: &FibNode<K,V>) -> bool {
+        self.key.eq(&other.key)
+    }
+}
+
+pub struct FibHeap<K,V> {
+    // The minimum element is always contained at the top of the first root.
+    roots: DList<FibEntry<K,V>>
+}
+
+impl<K: PartialOrd + Clone, V: Clone> FibHeap<K,V> {
     pub fn new() -> FibHeap<K,V> {
         FibHeap { roots: DList::new() }
     }
 
     // Must return a pointer to the specific entry for O(1) delete_node
     // and delete_key.
-    pub fn insert(&self, key: K, val: V) -> FibEntry<k,V> {
-
+    pub fn insert(&self, k: K, v: V) -> FibEntry<K,V> {
+        let node = FibNode {
+            children: DList::new(),
+            marked: false,
+            key: k,
+            value: v
+        };
+        let rc_node = Rc::new(node);
+        let ret = rc_node.clone();
+        let new_heap = FibHeap::new();
+        new_heap.roots.push(rc_node);
+        self.meld(new_heap);
+        ret
     }
     pub fn meld(&self, other: FibHeap<K,V>) -> FibHeap<K,V> {
+        if self.roots.is_empty() {
+            self.roots.append(other.roots);
+        } else if self.find_min().val0() <= other.find_min().val0() {
+            self.roots.append(other.roots);
+        } else {
+            self.roots.prepend(other.roots);
+        }
+        *self
+    }
+    pub fn find_min(&self) -> (K, V) {
+        match self.roots.front() {
+            Some(n) => (n.key.clone(), n.value.clone()),
+            None => fail!("Fibonacci heap is empty")
+        }
+    }
+    pub fn delete_min(&self) -> (K, V) {
+        match self.roots.pop_front() {
+            None => fail!("Fibonacci heap is empty"),
+            Some(min_node) => {
 
+            }
+        }
     }
 }
